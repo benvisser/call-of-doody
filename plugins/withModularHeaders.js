7e48d4d853be09c43ca4f697c972af6664283459
Ben Visser <benvisser@MacBook-Pro-3.local>
2026-02-03 22:05:33 -0500

    Disable New Architecture for @react-native-firebase compatibility

--- plugins/withModularHeaders.js
+++ plugins/withModularHeaders.js
@@ -1,7 +1,7 @@
 const { withPodfile } = require('@expo/config-plugins');
 
 /**
- * Config plugin to add use_modular_headers! and use_frameworks! for Firebase compatibility
+ * Config plugin to add use_modular_headers! for Firebase compatibility
  */
 const withModularHeaders = (config) => {
   return withPodfile(config, (config) => {
@@ -12,14 +12,6 @@ const withModularHeaders = (config) => {
       podfile = podfile.replace(
         /platform :ios.*\n/,
         (match) => `${match}\n# Enable modular headers globally for Firebase compatibility\nuse_modular_headers!\n`
-      );
-    }
-
-    // Add use_frameworks with static linkage for @react-native-firebase
-    if (!podfile.includes("use_frameworks! :linkage => :static")) {
-      podfile = podfile.replace(
-        /use_modular_headers!\n/,
-        (match) => `${match}\n# Use static frameworks for @react-native-firebase compatibility\nuse_frameworks! :linkage => :static\n`
       );
     }
 
